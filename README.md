a53d6499fa56eecce28544bb1e5f057b09739b6d
firemax13 <mydadisbrave2@gmail.com>
2021-01-05 12:40:38 +0800

    CameraMotor: Add Makefile Call
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- README.md
+++
@@ -1,2 +0,0 @@
-# samsung_crap_motor
-IDK why Samsung Galaxy A80 got crap motor in Prebuilt Vendor ROM's
---
+++ SamsungCameraMotor.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Samsung Camera Motor Path
+# SAMSUNG_CAMERA_MOTOR_PATH := device/samsung/camera/motor
+
+PRODUCT_PACKAGES += \
+    lineage.camera.motor@1.0-service.samsung
+
